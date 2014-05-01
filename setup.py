340833fa8940f0ad0fd553508ab12e82f15116ef
Rob Hoelz <rob@hoelz.ro>
2014-05-01 16:46:22 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='Spoke',
+      version='0.1',
+      description='API bindings for Spoke API',
+      author='Rob Hoelz',
+      author_email='rob.hoelz@skinnycorp.com',
+      url='https://github.com/Threadless/python-spoke',
+      packages=['spoke'],
+     )
