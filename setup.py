94b02e9e789cd469d92d0f8228b0408760bf6a33
Rob Hoelz <rob@hoelz.ro>
2014-05-02 11:21:25 -0500

    Convert to setuptools

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
-setup(name='Spoke',
-      version='0.1',
-      description='API bindings for Spoke API',
-      author='Rob Hoelz',
-      author_email='rob.hoelz@skinnycorp.com',
-      url='https://github.com/Threadless/python-spoke',
-      packages=['spoke'],
-     )
+setup(
+    name             = 'Spoke',
+    version          = '0.1',
+    packages         = find_packages(),
+    description      = 'API bindings for Spoke API',
+    license          = 'MIT',
+    author           = 'Rob Hoelz',
+    author_email     = 'rob.hoelz@skinnycorp.com',
+    url              = 'https://github.com/Threadless/python-spoke',
+    keywords         = 'spoke',
+    install_requires = ['lxml==3.3.5', 'requests==2.2.1'],
+    tests_require    = ['unittest2==0.5.1', 'nose==1.3.1', 'python-termstyle==0.1.10', 'rednose==0.4.1'],
+)
