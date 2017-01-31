8fe91cc44eb58f7cb15ef04607d21363260f3859
Dominic Bruno <dominicbruno@gmail.com>
2017-01-31 16:14:45 -0600

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name             = 'Python-Spoke',
-    version          = '1.0.8',
+    version          = '1.0.9',
     packages         = find_packages(),
     description      = 'API bindings for Spoke API',
     long_description = open(os.path.join(os.path.dirname(__file__), 'README.md'), 'r').read(),
